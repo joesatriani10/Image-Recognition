d2a3a104527ac5b8ed7395b245768b4bcc25023f
Winchester <Winchester@ALIEN>
2023-12-13 16:46:56 -0800

    Add project files.

---
+++ Image Recognition/Form1.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Image_Recognition
+{
+    partial class Main
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            pnlFormLoader = new Panel();
+            SuspendLayout();
+            // 
+            // pnlFormLoader
+            // 
+            pnlFormLoader.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlFormLoader.Location = new Point(232, 0);
+            pnlFormLoader.Margin = new Padding(3, 2, 3, 2);
+            pnlFormLoader.Name = "pnlFormLoader";
+            pnlFormLoader.Size = new Size(1138, 600);
+            pnlFormLoader.TabIndex = 0;
+            // 
+            // Main
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.FromArgb(81, 81, 81);
+            ClientSize = new Size(1370, 602);
+            Controls.Add(pnlFormLoader);
+            DoubleBuffered = true;
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Main";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Image Recognition";
+            WindowState = FormWindowState.Maximized;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel pnlFormLoader;
+    }
+}
---
+++ Image Recognition/Form1.cs
@@ -0,0 +1,10 @@
+namespace Image_Recognition
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+    }
+}
