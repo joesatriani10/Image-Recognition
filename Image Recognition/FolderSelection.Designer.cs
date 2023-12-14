be1803875e4e946e944a89bdeb353da9edae9d72
Angel Montes de Oca <joesatriani10@gmail.com>
2023-12-13 19:49:21 -0800

    Add new DataGridView and back panel for style

--- Image Recognition/FolderSelection.Designer.cs
+++ Image Recognition/FolderSelection.Designer.cs
@@ -32,17 +32,19 @@
             btnSelect = new Button();
             lblDisplay = new Label();
             dgFiles = new DataGridView();
+            pnlBack = new Panel();
             ((System.ComponentModel.ISupportInitialize)dgFiles).BeginInit();
+            pnlBack.SuspendLayout();
             SuspendLayout();
             // 
             // tbPath
             // 
             tbPath.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             tbPath.BorderStyle = BorderStyle.FixedSingle;
-            tbPath.Location = new Point(27, 30);
+            tbPath.Location = new Point(15, 49);
             tbPath.MinimumSize = new Size(0, 30);
             tbPath.Name = "tbPath";
-            tbPath.Size = new Size(642, 30);
+            tbPath.Size = new Size(654, 30);
             tbPath.TabIndex = 0;
             // 
             // btnSelect
@@ -52,9 +54,9 @@
             btnSelect.FlatAppearance.BorderSize = 0;
             btnSelect.FlatStyle = FlatStyle.Flat;
             btnSelect.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            btnSelect.Location = new Point(684, 30);
+            btnSelect.Location = new Point(675, 49);
             btnSelect.Name = "btnSelect";
-            btnSelect.Size = new Size(75, 30);
+            btnSelect.Size = new Size(86, 30);
             btnSelect.TabIndex = 1;
             btnSelect.Text = "Select";
             btnSelect.UseVisualStyleBackColor = false;
@@ -64,8 +66,8 @@
             // 
             lblDisplay.AutoSize = true;
             lblDisplay.Font = new Font("Segoe UI", 12F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            lblDisplay.ForeColor = Color.Gainsboro;
-            lblDisplay.Location = new Point(27, 63);
+            lblDisplay.ForeColor = Color.White;
+            lblDisplay.Location = new Point(15, 15);
             lblDisplay.MinimumSize = new Size(100, 30);
             lblDisplay.Name = "lblDisplay";
             lblDisplay.Size = new Size(170, 30);
@@ -82,12 +84,24 @@
             dgFiles.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             dgFiles.BorderStyle = BorderStyle.None;
             dgFiles.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
-            dgFiles.Location = new Point(27, 96);
+            dgFiles.Location = new Point(30, 96);
             dgFiles.MultiSelect = false;
             dgFiles.Name = "dgFiles";
             dgFiles.ReadOnly = true;
-            dgFiles.Size = new Size(732, 325);
+            dgFiles.Size = new Size(740, 325);
             dgFiles.TabIndex = 3;
+            // 
+            // pnlBack
+            // 
+            pnlBack.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlBack.BackColor = Color.Gray;
+            pnlBack.Controls.Add(btnSelect);
+            pnlBack.Controls.Add(tbPath);
+            pnlBack.Controls.Add(lblDisplay);
+            pnlBack.Location = new Point(12, 12);
+            pnlBack.Name = "pnlBack";
+            pnlBack.Size = new Size(776, 426);
+            pnlBack.TabIndex = 0;
             // 
             // FolderSelection
             // 
@@ -96,16 +110,15 @@
             BackColor = Color.FromArgb(81, 81, 81);
             ClientSize = new Size(800, 450);
             Controls.Add(dgFiles);
-            Controls.Add(lblDisplay);
-            Controls.Add(btnSelect);
-            Controls.Add(tbPath);
+            Controls.Add(pnlBack);
             FormBorderStyle = FormBorderStyle.None;
             Name = "FolderSelection";
             StartPosition = FormStartPosition.CenterParent;
             Text = "FolderSelection";
             ((System.ComponentModel.ISupportInitialize)dgFiles).EndInit();
+            pnlBack.ResumeLayout(false);
+            pnlBack.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -114,5 +127,6 @@
         private Button btnSelect;
         private Label lblDisplay;
         private DataGridView dgFiles;
+        private Panel pnlBack;
     }
 }
--- Image Recognition/ResultsGrid.Designer.cs
+++ Image Recognition/ResultsGrid.Designer.cs
@@ -29,17 +29,47 @@
         private void InitializeComponent()
         {
             label1 = new Label();
+            dataGridView1 = new DataGridView();
+            pnlBack = new Panel();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            pnlBack.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 24F);
-            label1.Location = new Point(364, 187);
+            label1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(15, 15);
             label1.Name = "label1";
-            label1.Size = new Size(80, 45);
+            label1.Size = new Size(100, 21);
             label1.TabIndex = 0;
-            label1.Text = "Grid";
+            label1.Text = "Results Grid";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.AllowUserToAddRows = false;
+            dataGridView1.AllowUserToDeleteRows = false;
+            dataGridView1.AllowUserToOrderColumns = true;
+            dataGridView1.AllowUserToResizeColumns = false;
+            dataGridView1.AllowUserToResizeRows = false;
+            dataGridView1.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            dataGridView1.BorderStyle = BorderStyle.None;
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(30, 96);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(740, 325);
+            dataGridView1.TabIndex = 1;
+            // 
+            // pnlBack
+            // 
+            pnlBack.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pnlBack.BackColor = Color.Gray;
+            pnlBack.Controls.Add(label1);
+            pnlBack.Location = new Point(12, 12);
+            pnlBack.Name = "pnlBack";
+            pnlBack.Size = new Size(776, 426);
+            pnlBack.TabIndex = 2;
             // 
             // ResultsGrid
             // 
@@ -47,16 +77,21 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(81, 81, 81);
             ClientSize = new Size(800, 450);
-            Controls.Add(label1);
+            Controls.Add(dataGridView1);
+            Controls.Add(pnlBack);
             FormBorderStyle = FormBorderStyle.None;
             Name = "ResultsGrid";
             Text = "ResultsGrid";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            pnlBack.ResumeLayout(false);
+            pnlBack.PerformLayout();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
 
         private Label label1;
+        private DataGridView dataGridView1;
+        private Panel pnlBack;
     }
 }
