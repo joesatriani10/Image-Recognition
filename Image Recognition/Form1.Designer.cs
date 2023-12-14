71d0a30a3988737509958b7b6bff9b1a05a93409
Angel Montes de Oca <joesatriani10@gmail.com>
2023-12-13 17:15:01 -0800

    Created Panel Nav feature

--- Image Recognition/Form1.Designer.cs
+++ Image Recognition/Form1.Designer.cs
@@ -30,6 +30,17 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
             pnlFormLoader = new Panel();
+            pnlMenu = new Panel();
+            pnlUser = new Panel();
+            label2 = new Label();
+            lblUser = new Label();
+            btnFolderSelect = new Button();
+            btnGrid = new Button();
+            btnThumbnails = new Button();
+            btnSettings = new Button();
+            pnlNav = new Panel();
+            pnlMenu.SuspendLayout();
+            pnlUser.SuspendLayout();
             SuspendLayout();
             // 
             // pnlFormLoader
@@ -41,12 +52,118 @@
             pnlFormLoader.Size = new Size(1138, 600);
             pnlFormLoader.TabIndex = 0;
             // 
+            // pnlMenu
+            // 
+            pnlMenu.BackColor = Color.FromArgb(57, 57, 57);
+            pnlMenu.Controls.Add(pnlNav);
+            pnlMenu.Controls.Add(btnSettings);
+            pnlMenu.Controls.Add(btnThumbnails);
+            pnlMenu.Controls.Add(btnGrid);
+            pnlMenu.Controls.Add(btnFolderSelect);
+            pnlMenu.Controls.Add(pnlUser);
+            pnlMenu.Dock = DockStyle.Left;
+            pnlMenu.Location = new Point(0, 0);
+            pnlMenu.Margin = new Padding(3, 2, 3, 2);
+            pnlMenu.Name = "pnlMenu";
+            pnlMenu.Size = new Size(233, 602);
+            pnlMenu.TabIndex = 1;
+            // 
+            // pnlUser
+            // 
+            pnlUser.BackColor = Color.DimGray;
+            pnlUser.Controls.Add(label2);
+            pnlUser.Controls.Add(lblUser);
+            pnlUser.Dock = DockStyle.Top;
+            pnlUser.Location = new Point(0, 0);
+            pnlUser.Name = "pnlUser";
+            pnlUser.Size = new Size(233, 100);
+            pnlUser.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(96, 58);
+            label2.Name = "label2";
+            label2.Size = new Size(0, 15);
+            label2.TabIndex = 1;
+            // 
+            // lblUser
+            // 
+            lblUser.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            lblUser.AutoSize = true;
+            lblUser.Location = new Point(81, 58);
+            lblUser.Name = "lblUser";
+            lblUser.Size = new Size(60, 15);
+            lblUser.TabIndex = 0;
+            lblUser.Text = "Username";
+            // 
+            // btnFolderSelect
+            // 
+            btnFolderSelect.Dock = DockStyle.Top;
+            btnFolderSelect.FlatStyle = FlatStyle.Flat;
+            btnFolderSelect.ForeColor = Color.Gainsboro;
+            btnFolderSelect.Location = new Point(0, 100);
+            btnFolderSelect.Name = "btnFolderSelect";
+            btnFolderSelect.Size = new Size(233, 55);
+            btnFolderSelect.TabIndex = 1;
+            btnFolderSelect.Text = "Folder Selection";
+            btnFolderSelect.TextAlign = ContentAlignment.MiddleLeft;
+            btnFolderSelect.UseVisualStyleBackColor = true;
+            btnFolderSelect.Click += btnFolderSelect_Click;
+            // 
+            // btnGrid
+            // 
+            btnGrid.Dock = DockStyle.Top;
+            btnGrid.FlatStyle = FlatStyle.Flat;
+            btnGrid.ForeColor = Color.Gainsboro;
+            btnGrid.Location = new Point(0, 155);
+            btnGrid.Name = "btnGrid";
+            btnGrid.Size = new Size(233, 55);
+            btnGrid.TabIndex = 2;
+            btnGrid.Text = "Results Grid";
+            btnGrid.TextAlign = ContentAlignment.MiddleLeft;
+            btnGrid.UseVisualStyleBackColor = true;
+            // 
+            // btnThumbnails
+            // 
+            btnThumbnails.Dock = DockStyle.Top;
+            btnThumbnails.FlatStyle = FlatStyle.Flat;
+            btnThumbnails.ForeColor = Color.Gainsboro;
+            btnThumbnails.Location = new Point(0, 210);
+            btnThumbnails.Name = "btnThumbnails";
+            btnThumbnails.Size = new Size(233, 55);
+            btnThumbnails.TabIndex = 3;
+            btnThumbnails.Text = "Results Thubnails";
+            btnThumbnails.TextAlign = ContentAlignment.MiddleLeft;
+            btnThumbnails.UseVisualStyleBackColor = true;
+            // 
+            // btnSettings
+            // 
+            btnSettings.Dock = DockStyle.Bottom;
+            btnSettings.FlatStyle = FlatStyle.Flat;
+            btnSettings.ForeColor = Color.Gainsboro;
+            btnSettings.Location = new Point(0, 547);
+            btnSettings.Name = "btnSettings";
+            btnSettings.Size = new Size(233, 55);
+            btnSettings.TabIndex = 4;
+            btnSettings.Text = "Settings";
+            btnSettings.UseVisualStyleBackColor = true;
+            // 
+            // pnlNav
+            // 
+            pnlNav.BackColor = Color.Gainsboro;
+            pnlNav.Location = new Point(0, 108);
+            pnlNav.Name = "pnlNav";
+            pnlNav.Size = new Size(4, 128);
+            pnlNav.TabIndex = 0;
+            // 
             // Main
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(81, 81, 81);
             ClientSize = new Size(1370, 602);
+            Controls.Add(pnlMenu);
             Controls.Add(pnlFormLoader);
             DoubleBuffered = true;
             Icon = (Icon)resources.GetObject("$this.Icon");
@@ -54,11 +171,23 @@
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Image Recognition";
             WindowState = FormWindowState.Maximized;
+            pnlMenu.ResumeLayout(false);
+            pnlUser.ResumeLayout(false);
+            pnlUser.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private Panel pnlFormLoader;
+        private Panel pnlMenu;
+        private Panel pnlUser;
+        private Label label2;
+        private Label lblUser;
+        private Button btnFolderSelect;
+        private Button btnThumbnails;
+        private Button btnGrid;
+        private Panel pnlNav;
+        private Button btnSettings;
     }
 }
--- Image Recognition/Form1.cs
+++ Image Recognition/Form1.cs
@@ -2,9 +2,66 @@ namespace Image_Recognition
 {
     public partial class Main : Form
     {
+        private Button? _lastClicked;
+
         public Main()
         {
             InitializeComponent();
+
+            // Set the first button to be selected
+            GetPnlNav(btnFolderSelect);
         }
+
+        private void btnFolderSelect_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void RefreshPnlNav()
+        {
+            if (_lastClicked != null)
+            {
+                pnlNav.Hide();
+                pnlNav.Height = _lastClicked.Height;
+                pnlNav.Top = _lastClicked.Top;
+                pnlNav.Left = _lastClicked.Left;
+                pnlNav.Show();
+            }
+        }
+
+        private void GetPnlNav(Button? button)
+        {
+            _lastClicked = button;
+            // Set buttons to normal color
+
+            RestoreButtonColors();      
+
+            if(button != null)
+            {
+                button.BackColor = Color.FromArgb(81, 81, 81);
+                button.ForeColor = Color.FromArgb(255, 131, 131);
+            }
+            
+            
+            // Set pnlNav to button
+            RefreshPnlNav();
+        }
+
+
+        private void RestoreButtonColors()
+        {
+            SetButtonColors(btnFolderSelect);
+            SetButtonColors(btnGrid);
+            SetButtonColors(btnThumbnails);
+            SetButtonColors(btnSettings);
+
+        }
+
+        private void SetButtonColors(Button button)
+        {
+            button.BackColor = Color.FromArgb(57, 57, 57);
+            button.ForeColor = Color.Gainsboro;
+        }
+
     }
 }
